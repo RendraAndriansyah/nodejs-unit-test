10f23198a21d240f4bfcc8d64217bbb9b62f2f6c
travelite <rendrabootcamp@gmail.com>
2023-04-24 05:41:10 +0700

    setup & install babel

--- jest/sum.test.js
+++ jest/sum.test.js
@@ -1,6 +1,8 @@
 import { sum } from "../src/sum";
 
 test("test sum function", () => {
-	const result = sum(2, 5);
-	expect(result).toBe(5);
+	expect(sum(2, 3)).toBe(5);
+});
+test("test sum function", () => {
+	expect(sum(4, 4)).toEqual(8);
 });
